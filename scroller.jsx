bc1624a6779b6e53a338ccbcd9d003a7d6c9cc81
Hitesh saini <128598996+hitesh09saini@users.noreply.github.com>
2024-09-01 19:17:27 +0530

    Create scroller.jsx

---
+++ scroller.jsx
@@ -0,0 +1,57 @@
+
+      <div className="fixed inset-0 flex items-center -rotate-2 select-none">
+        <div className="relative">
+          <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
+            <h1 className="shrink-0 text-white text-10xl font-black ">
+              Wawa Sensei
+            </h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">
+              React Three Fiber
+            </h2>
+            <h2 className="shrink-0 text-white text-12xl font-bold">
+              Three.js
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Ultimate Guide
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">
+              Tutorials
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+              Learn
+            </h2>
+            <h2 className="shrink-0 text-white text-13xl font-bold">
+              Practice
+            </h2>
+            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+              Creative
+            </h2>
+          </div>
+          <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
+            <h1 className="shrink-0 text-white text-10xl font-black ">
+              Wawa Sensei
+            </h1>
+            <h2 className="shrink-0 text-white text-8xl italic font-light">
+              React Three Fiber
+            </h2>
+            <h2 className="shrink-0 text-white text-12xl font-bold">
+              Three.js
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Ultimate Guide
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">
+              Tutorials
+            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
+              Learn
+            </h2>
+            <h2 className="shrink-0 text-white text-13xl font-bold">
+              Practice
+            </h2>
+            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
+              Creative
+            </h2>
+          </div>
+        </div>
+      </div>
